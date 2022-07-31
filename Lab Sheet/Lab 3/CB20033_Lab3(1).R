0cd125cad2b7471356ea7e19779ee843f57d3fd0
Zhafran Zainal <73966883+zhafranzainal@users.noreply.github.com>
2022-07-31 14:04:25 +0800

    Create CB20033_Lab3(1).R

---
+++ Lab Sheet/Lab 3/CB20033_Lab3(1).R
@@ -0,0 +1,8 @@
+#Q1
+A <- unlist(list('Farah', 'Jie', 'Logathepan', 'Zhafran', 'Hin', 'Thaqiyuddin', 'Chin', 'Fatihah', 'Han', 'Shun', 'Chyin'))
+B <- cbind(c(3,3,23), c(8,10,9), c(33,8,-5))
+C <- array(c(1:10), dim=c(3, 5, 3), dimnames=list(c('A', 'B', 'C'), c('d', 'e', 'f', 'g', 'h'), c('Alyssa', 'Shim', 'Yi')))
+
+Chuan <- list(A, B, C)
+
+Chuan
